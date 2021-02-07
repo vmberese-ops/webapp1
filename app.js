47052f285dc4805133d5522a99d2af65bf57a95b
vmberese-ops <vmberese@addu.edu.ph>
2021-02-07 17:08:41 +0800

    fixed home

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
     let urll = './views/';
 
-    if(req.url == '/home') {
+    if(req.url == '/' || req.url == '/home') {
         urll += 'index.html';
         res.statusCode = 200;
     }
